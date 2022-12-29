d0dcfd9bb34500d99bb71ed4d1d30426f4db1b67
Stenli Suryadinata <47641964+stenlisuryadinata@users.noreply.github.com>
2022-12-29 07:38:39 +0700

    Create pattern.js

---
+++ getting-started-with-javascript-master/src/pattern.js
@@ -0,0 +1,47 @@
+/*
+
+The drawPattern() function should accept number of rows as input.
+
+The function should return string that contains the pyramid structure for the number of rows inputted.
+
+The pyramid structure should have the following pattern:
+
+        *
+       * *
+      * * *
+     * * * *
+    * * * * *
+
+The function should return error message "Invalid Input Type, Row Input Should Be of Type Number !!", 
+if non-numeric value is passed to the function.
+
+*/
+
+module.exports = function drawPattern(n) {
+
+  // Provide Solution Code Here
+  if(typeof n == 'number'){
+    let string ="";
+    
+    // External loop
+    for(let i=1; i<= n; i++){
+    // printing spaces
+    for(let j=1; j<=( n - i); j++){
+        string += " "; 
+    }
+    //printing star
+    for(let k=1; k<=i; k++){
+        string += "* ";
+    }
+    string += "\n"; 
+}
+return(string );
+  }else {
+
+  //console.log("Invalid Input Type, Row Input Should Be of Type Number !!");
+  
+  return "Invalid Input Type, Row Input Should Be of Type Number !!";
+  }
+}
+
+
